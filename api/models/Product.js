dd036cc3be539f95dd1196a1dd356ae837a09421
maguiromeroa <maguiromeroa@gmail.com>
2023-01-04 00:02:02 +0100

    Product model done

--- api/models/Product.js
+++ api/models/Product.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const uniqueValidator = require("mongoose-unique-validator");
 
-const userSchema = new Schema({
+const productSchema = new Schema({
   name: {
     type: String,
     unique: true,
@@ -29,4 +29,4 @@ const userSchema = new Schema({
 
 UserSchema.plugin(uniqueValidator, { message: "is already taken." });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Product", productSchema);
