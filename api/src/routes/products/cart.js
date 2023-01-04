b25ece713dfbb84bc538d88e445174ad872b1b1c
maguiromeroa <maguiromeroa@gmail.com>
2023-01-04 13:04:10 +0100

    Cart route has been created

---
+++ api/src/routes/products/cart.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const User = require("../../models/User");
+
+const cart = Router();
+
+cart.get("/carrito", async (user_id) => {
+  try {
+    const userCart = await User.find({
+      user: user_id,
+    });
+    res.status(200).send(userCart.cart);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+module.exports = cart;
