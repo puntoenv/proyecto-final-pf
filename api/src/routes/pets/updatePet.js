9efd2800d44e0b91e392e293c6257f20e004c39b
gitperalta <sebssjb@gmail.com>
2023-01-04 12:12:05 -0500

    correcciones

--- api/src/routes/pets/updatePet.js
+++ api/src/routes/pets/updatePet.js
@@ -1,28 +1,28 @@
-const { Router } = require("express");
-const Pet = require("../../models/Pet");
-const updatePet = Router();
+// const { Router } = require("express");
+// const Pet = require("../../models/Pet");
+// const updatePet = Router();
 
-updatePet.put("/:id", async (req, res) => {
-  try {
-    let { id } = req.params;
-    let {
-      name,
-      size,
-      age,
-      description,
-      image,
-      type,
-      gender,
-      location,
-      userId,
-    } = req.body;
-    let pet = Pet.findById(id);
-    pet.name = name ? name : pet.name;
-    pet["size"] = size ? size : pet["size"];
-    pet.age = age ? age : pet.age;
-    pet.name = name ? name : pet.name;
-    pet.name = name ? name : pet.name;
-  } catch (error) {}
-});
+// updatePet.put("/:id", async (req, res) => {
+//   try {
+//     let { id } = req.params;
+//     let {
+//       name,
+//       size,
+//       age,
+//       description,
+//       image,
+//       type,
+//       gender,
+//       location,
+//       userId,
+//     } = req.body;
+//     let pet = Pet.findById(id);
+//     pet.name = name ? name : pet.name;
+//     pet["size"] = size ? size : pet["size"];
+//     pet.age = age ? age : pet.age;
+//     pet.description = name ? name : pet.name;
+//     pet.name = name ? name : pet.name;
+//   } catch (error) {}
+// });
 
-module.exports = updatePet;
+// module.exports = updatePet;
