1c276b72514792efbd1183fd4192206011289d08
Ezequiel Gómez Merlo <gomezmerloe@gmail.com>
2023-01-04 12:16:59 -0300

    added route post auth, just user register

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const detail = require("./detail.js");
 const allPets = require("./allPets.js");
 const postPet = require("./postPet.js");
+const routeAuth = require("./routeAuth");
 
 router.use("/detail", detail);
 router.use("/pets", allPets);
 router.use("/postPet", postPet);
+router.use("/auth", routeAuth);
 
 module.exports = router;
