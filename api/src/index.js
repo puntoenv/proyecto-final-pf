bc892761ed2a9c509265d073d9144c90e78b4185
Ezequiel Gómez Merlo <gomezmerloe@gmail.com>
2023-01-04 13:20:38 -0300

    added auth user register

--- api/src/index.js
+++ api/src/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const detail = require("./detail.js");
 const allPets = require("./allPets.js");
 const postPet = require("./postPet.js");
+const routeAuth = require("./routes/Auth/routeAuth");
 
 router.use("/detail", detail);
 router.use("/pets", allPets);
