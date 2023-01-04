a9b84e8e83630c3166443c852c3a336b5a1a4e66
maguiromeroa <maguiromeroa@gmail.com>
2023-01-04 22:47:12 +0100

    Borrado lógico para usuario. También se hizo borrado lógico para el detalle de la mascota (solo para mayor seguridad)

--- api/src/routes/pets/detail.js
+++ api/src/routes/pets/detail.js
@@ -5,7 +5,7 @@ const detail = express.Router();
 detail.get("/:id", async (req, res) => {
   try {
     let { id } = req.params;
-    let detail = await Pet.findById(id);
+    let detail = await Pet.find({ _id: id, hidden: false });
     res.status(200).send(detail);
   } catch (error) {
     res.status(400).send(error.message);
