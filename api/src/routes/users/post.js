74646d2ec6b6314488c1803e079d6f4580a7e99c
gitperalta <sebssjb@gmail.com>
2023-01-04 16:34:11 -0500

    correccion

---
+++ api/src/routes/users/post.js
@@ -0,0 +1,8 @@
+// const { Router } = require("express");
+// const postUser = Router();
+
+// postUser.post("/", async (req, res) => {
+//   let { name } = req.body;
+// });
+
+// module.exports = postUser;
