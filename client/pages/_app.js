41342ba093d3b24b73561ff6abe7c19cea7f57ae
brayanmar <101063730+brayanmar@users.noreply.github.com>
2023-01-04 13:13:28 -0500

    b

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import {SessionProvider} from 'next-auth/react'
+import { store } from '../stores/store'
+import { Provider } from 'react-redux'
 
 export default function App({ Component, pageProps, session }) {
   return (
     <SessionProvider session={session}> 
+    <Provider store={store}>
       <Component {...pageProps} />
+      </Provider>
     </SessionProvider>
   )
 }
