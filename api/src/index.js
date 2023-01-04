66827573b5850203228114c36295e2166fedd64a
Ezequiel Gómez Merlo <gomezmerloe@gmail.com>
2023-01-04 18:43:07 -0300

    changed names routes pets

--- api/src/index.js
+++ api/src/index.js
@@ -5,9 +5,7 @@ const allPets = require("./routes/pets/allPets");
 const postPet = require("./routes/pets/postPet");
 const routeAuth = require("./routes/Auth/routeAuth");
 
-router.use("/detail", detail);
-router.use("/pets", allPets);
-router.use("/postPet", postPet);
+router.use("/pets", detail, allPets, postPet);
 router.use("/auth", routeAuth);
 
 module.exports = router;
