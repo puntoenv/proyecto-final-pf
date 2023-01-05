86ee59f9b20a1ead2fe5dfc4eabe40c90fb5367a
Ezequiel Gómez Merlo <gomezmerloe@gmail.com>
2023-01-05 11:51:41 -0300

    added auth credencials, jwt, validations user register and login + mdw of jwt

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
+
 const detail = require("./routes/pets/detail");
 const allPets = require("./routes/pets/allPets");
 const postPet = require("./routes/pets/postPet");
 const routeAuth = require("./routes/Auth/routeAuth");
+const verifyAuth = require("../middlewares/mw-jwt");
 
-router.use("/pets", detail, allPets, postPet);
+// AGREGAR PARA EL PRIMER SPRINT EL VERIFY AUTH PARA LAS SIGUIENTES RUTAS
+/* RUTAS SEGURAS
+
+detail pet
+detail produt
+profile user
+update pet
+compra producto
+post pet
+
+*/
+
+router.use("/pets", allPets);
+router.use("/pets", verifyAuth, detail);
+router.use("/pets", verifyAuth, postPet);
 router.use("/auth", routeAuth);
+router.use("products");
 
 module.exports = router;
