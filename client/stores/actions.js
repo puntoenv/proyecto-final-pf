34646cb86a5b080f26b51832c1bb2ee4aee0f755
Emiliano1128 <emibrito100@icloud.com>
2023-01-06 16:16:57 -0300

    try catch en actions

--- client/stores/actions.js
+++ client/stores/actions.js
@@ -3,29 +3,45 @@ import { getPersonajes, getmunicipios } from "./slice";
 import { getMascotas /* getMascotaEncontrada */ } from "./mascotas";
 
 export const getper = () => async (dispatch) => {
-  await fetch("https://apis.datos.gob.ar/georef/api/provincias")
-    .then((res) => (res.ok ? res.json() : promise.reject(res)))
-    .then((res) => dispatch(getPersonajes(res)));
+  try {
+    await fetch("https://apis.datos.gob.ar/georef/api/provincias")
+      .then((res) => (res.ok ? res.json() : promise.reject(res)))
+      .then((res) => dispatch(getPersonajes(res)));
+  } catch (error) {
+    return console.error(error);
+  }
 };
 
 export const getmuni = (municipios) => async (dispatch) => {
-  await fetch(
-    `https://apis.datos.gob.ar/georef/api/municipios?provincia=${municipios}`
-  )
-    .then((res) => (res.ok ? res.json() : promise.reject(res)))
-    .then((res) => dispatch(getmunicipios(res)));
+  try {
+    await fetch(
+      `https://apis.datos.gob.ar/georef/api/municipios?provincia=${municipios}`
+    )
+      .then((res) => (res.ok ? res.json() : promise.reject(res)))
+      .then((res) => dispatch(getmunicipios(res)));
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getPets = () => async (dispatch) => {
-  let allPets = await axios("http://localhost:3001/pets/all");
-  dispatch(getMascotas(allPets.data.pets));
+  try {
+    let allPets = await axios("http://localhost:3001/pets/all");
+    dispatch(getMascotas(allPets.data.pets));
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const searchPet = (pet) => async (dispatch) => {
-  const petEncontrado = await axios(
-    `http://localhost:3001/pets/by-name?name=${pet}`
-  );
-  dispatch(getMascotas(petEncontrado.data));
+  try {
+    const petEncontrado = await axios(
+      `http://localhost:3001/pets/by-name?name=${pet}`
+    );
+    dispatch(getMascotas(petEncontrado.data));
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 /*export const filtersize = async (fil) => {
@@ -35,9 +51,13 @@ export const searchPet = (pet) => async (dispatch) => {
 }*/
 
 export const PostAdop = async (post) => {
-  console.log(post);
-  let res = await axios.post("http://localhost:3001/pets/post-pet", post);
-  return res;
+  try {
+    console.log(post);
+    let res = await axios.post("http://localhost:3001/pets/post-pet", post);
+    return res;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const postUser = (payload) => {
