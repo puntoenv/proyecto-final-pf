86b633258d90e67ff3a3da2b7eaf6b1411fd6735
gitperalta <sebssjb@gmail.com>
2023-01-07 14:22:03 -0500

    Detalle

--- client/pages/detail/[id].jsx
+++ client/pages/detail/[id].jsx
@@ -1,45 +1,16 @@
 import Image from "next/image";
 import styles from "./detail.module.css";
+import NavBar from "../../components/NavBar/NavBar";
 
 export default function Detail({ data }) {
   return (
     <>
+      <NavBar />
       <div className={styles.container}>
+        <div className={styles.title}>
+          <h1> {data.name.toUpperCase()} </h1>
+        </div>
         <div className={styles.detail}>
-          <div className={styles.text}>
-            <div>
-              <h4> Nombre: </h4>
-              <span> {data.name} </span>
-            </div>
-            <div>
-              <h4> Edad: </h4>
-              <span> {data.age} </span>
-            </div>
-            <div>
-              <h4> Tamaño: </h4>
-              <span> {data.size} </span>
-            </div>
-            <div>
-              <h4> Sexo: </h4>
-              <span> {data.gender} </span>
-            </div>
-            <div>
-              <h4> Descripción: </h4>
-              <span> {data.description} </span>
-            </div>
-            <div>
-              <h4>Ubicación</h4>
-              <span>Provincia: {data.location.provincia} </span>
-              <span> Municipio: {data.location.municipio}</span>
-            </div>
-            <div>
-              <h4>Especie: </h4>
-              <span>{data.type}</span>
-            </div>
-            <div>
-              <h4></h4>
-            </div>
-          </div>
           <Image
             className={styles.image}
             width="1000"
@@ -47,6 +18,33 @@ export default function Detail({ data }) {
             src={data.image}
             alt="Imagen de la mascota."
           />
+          <div className={styles.text}>
+            <div>
+              <h4> DESCRIPCIÓN </h4>
+              <span> {data.description} </span>
+            </div>
+            <div>
+              <h4> EDAD </h4>
+              <span> {data.age} años</span>
+            </div>
+            <div>
+              <h4> TAMAÑO </h4>
+              <span> {data.size} </span>
+            </div>
+            <div>
+              <h4> GENERO </h4>
+              <span> {data.gender} </span>
+            </div>
+            <div>
+              <h4> UBICACIÓN </h4>
+              <span>Provincia: {data.location.provincia} </span>
+              <span> Municipio: {data.location.municipio}</span>
+            </div>
+            <div>
+              <h4> ESPECIE </h4>
+              <span>{data.type}</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
