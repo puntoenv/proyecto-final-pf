83d4f9399be2caf31bda8c94a1d91fed35547b8b
gitperalta <sebssjb@gmail.com>
2023-01-07 11:27:31 -0500

    Errores

--- client/components/Card/index.js
+++ client/components/Card/index.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 
-
 export default function Card({
   id,
   nombre,
@@ -11,12 +10,12 @@ export default function Card({
   tamaño,
   tipo,
   locacion,
+  description,
 }) {
   return (
     <div>
-       <Link href={`/detail/${id}`}>
-        
-      <h3>Nombre: {nombre}</h3>
+      <Link href={`/detail/${id}`}>
+        <h3>Nombre: {nombre}</h3>
       </Link>
       <img src={imagen} width={100} height={100} alt="imagen de la mascota" />
       <p>Edad: {edad}</p>
@@ -28,7 +27,7 @@ export default function Card({
       {locacion?.lat ? <p>Latitud: {locacion.lat}</p> : null}
       {locacion?.provincia ? <p>Provincia: {locacion.provincia}</p> : null}
       {locacion?.municipio ? <p>Municipio: {locacion?.municipio}</p> : null}
-      
+      <p>Descripcion: {description}</p>
     </div>
   );
 }
