d517a8429ed0892c3817c0ea5d9c8aacf7abf181
brayanmar <101063730+brayanmar@users.noreply.github.com>
2023-01-07 11:53:47 -0500

    modificacion de paginado

--- client/components/paginated/pagina.js
+++ client/components/paginated/pagina.js
@@ -1,16 +1,86 @@
 import styles from "./style.module.css";
-function Pagina({pets, pg, page}) {
+function Pagina({
+  pets,
+  pg,
+  page,
+  onPrevClick,
+  onNextClick,
+  curren,
+  maxPageLimit,
+  minPageLimit,
+}) {
   let numeroPaginas = [];
-  for (let i = 0; i < (pets.length / pg); i++) {
-    numeroPaginas.push(i + 1);
+  for (let i = 0; i < pets.length - 1 / pg; i++) {
+    numeroPaginas.push(i);
   }
-   
+  const handlePrevClick = () => {
+    onPrevClick();
+  };
+
+  const handleNextClick = () => {
+    onNextClick();
+  };
+
+  const handlePageClick = (e) => {
+    page(Number(e.target.id));
+  };
+  const pageNumbers = numeroPaginas.map((page) => {
+    if (page <= maxPageLimit && page > minPageLimit) {
+      return (
+        <li
+          key={page}
+          id={page}
+          onClick={handlePageClick}
+          className={curren === page ? "active" : null}
+        >
+          {page}
+        </li>
+      );
+    } else {
+      return null;
+    }
+  });
+  let pageIncrementEllipses = null;
+  if (numeroPaginas.length > maxPageLimit) {
+    pageIncrementEllipses = (
+      <button className={styles.button} onClick={handleNextClick}>
+        {numeroPaginas}
+      </button>
+    );
+  }
+  let pageDecremenEllipses = null;
+  if (minPageLimit >= 1) {
+    pageDecremenEllipses = (
+      <button className={styles.button} onClick={handlePrevClick}>
+        {numeroPaginas}
+      </button>
+    );
+  }
+
   return (
     <div>
-      {numeroPaginas.map(a => (<button className={styles.button} key={a} onClick={() => page(a)}>{a}</button>))}
+      <button
+        className={styles.button}
+        onClick={handlePrevClick}
+        disabled={curren === numeroPaginas[0]}
+      >
+        Prev
+      </button>
+      {pageDecremenEllipses}
+      {pageNumbers}
+      {pageIncrementEllipses}
+      <button
+        className={styles.button}
+        onClick={handleNextClick}
+        disabled={curren === numeroPaginas[numeroPaginas.length - 1]}
+      >
+        Next
+      </button>
       pagina
-      </div>
-  )
+    </div>
+  );
 }
 
-export default Pagina
+export default Pagina;
+
+/*  {numeroPaginas.map(a => (<button className={styles.button} key={a} onClick={() => page(a)}>{a}</button>))}*/
