232fb322a3f6278bfc334fd2be71f57cb0cbf15b
Miranda <mirufuertes@gmail.com>
2023-01-08 17:59:51 -0300

    carrusel finalizado

--- client/pages/home/index.jsx
+++ client/pages/home/index.jsx
@@ -7,9 +7,6 @@ import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/footer";
 import Nosotros from "../../components/infoHome/nosotros";
 import Layout from "../layout";
-// import ProductSliderEshop from "../../components/ProductSlider/ProductSliderEshop";
-//const inter = Inter({ subsets: ['latin'] })
-// import ProductSlider from "../../components/ProductSlider/ProductSlider";
 import home from "../../img/prueba.jpeg";
 import logo from "../../img/logo.jpeg";
 import Cat from "../../img/meal.png";
@@ -20,50 +17,81 @@ import Alim from "../../img/alim.png";
 import Al from "../../img/al.png";
 import Ali from "../../img/ali.png";
 import Alimen from "../../img/alimen.png";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import PetsCard from "../../components/Carousel/petsCard";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPets } from "../../stores/actions";
+import React, { Component } from "react";
 
 export default function Home() {
-  return (
-    <>
-      <Layout title="Inicio" />
-      <div className={styles.home}>
-        <NavBar />
-        <Link href={"/home"} classname="logo">
-          <Image
-            src={logo}
-            alt="logo"
-            className={styles.logo}
-            width="auto"
-            height="auto"
-          />
-        </Link>
-        <p className={styles.friend}>Encuentra a tu nuevo mejor amigo</p>
-        <Image
-          src={home}
-          alt="home"
-          className={styles.home}
-          width="auto"
-          height="auto"
-        />
-        <div className={styles.barGreen}></div>
-        <div className={styles.divAlimento}></div>
-        {/* <span className={styles.catAlimento}></span> */}
-        <div className={styles.containKitten}>
-          <Image
-            src={Cat}
-            alt="kitten"
-            className={styles.kitten}
-            width="auto"
-            height="auto"
-          />
-        </div>
+  {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+    };
 
-        <Nosotros />
+    const data = useSelector((data) => data.mascotas.mascotas);
 
-        <div className={styles.carruselAnimals}>
-          <Link className={styles.linkAdoptar} href="/petsPosts">
-            <h1>¡Adoptame! --- (acá abajo va el carrousel)</h1>
+    const dispatch = useDispatch();
+    useEffect(() => {
+      dispatch(getPets());
+    }, []);
+
+    return (
+      <>
+        <Layout title="Inicio" />
+        <div className={styles.home}>
+          <NavBar />
+          <Link href={"/home"} classname="logo">
+            <Image
+              src={logo}
+              alt="logo"
+              className={styles.logo}
+              width="auto"
+              height="auto"
+            />
           </Link>
-          {/* <ProductSlider /> */}
+          <p className={styles.friend}>Encuentra a tu nuevo mejor amigo</p>
+          <Image
+            src={home}
+            alt="home"
+            className={styles.home}
+            width="auto"
+            height="auto"
+          />
+          <div className={styles.barGreen}></div>
+          <div className={styles.divAlimento}></div>
+          {/* <span className={styles.catAlimento}></span> */}
+          <div className={styles.containKitten}>
+            <Image
+              src={Cat}
+              alt="kitten"
+              className={styles.kitten}
+              width="auto"
+              height="auto"
+            />
+          </div>
+
+          <Nosotros />
+
+          
+            <Slider {...settings}>
+              {data.slice(0, 9).map((mascota) => (
+                <PetsCard
+                  key={mascota._id}
+                  nombre={mascota.name}
+                  imagen={mascota.image}
+                  genero={mascota.gender}
+                />
+              ))}
+            </Slider>
+            <div className={styles.containerAdopciones}>
           <h1 className={styles.tituloAdopcion}>Info Adopciones</h1>
           <p className={styles.infoAdopcion}>
             Nuestra plataforma permite a los usuarios visualizar todas las
@@ -73,44 +101,12 @@ export default function Home() {
             adoptar a un nuevo integrante. También esperamos compromiso para con
             los usuarios que llevan a cabo las publicaciones de los posteos.
           </p>
+          </div>
           <Join />
 
-          <div className={styles.carruselEshop}>
-            {/* <Link className={styles.linkEshop} href="/eshop">
-              <h1>E-shop</h1>
-            </Link> */}
-            {/* <ProductSliderEshop /> */}
-          </div>
           <Footer />
         </div>
-
-        {/* <div>
-          <form className={styles.searchBar}>
-            <input
-              className={styles.inputSearch}
-              type="text"
-              placeholder="Buscador..."
-            />
-            <button className={styles.buttonSearch} type="submit">
-              Buscar
-            </button>
-          </form>
-        </div>
-
-        <p className={styles.adopcion}> Parrafo sobre adopcion</p>
-
-        <div className={styles.sobreNosotros}>
-          <p className={styles.textoNosotros}>
-            Nosotros somos Little Paws y nuestro objetivo es poder buscarles un
-            hogar y buenas condiciones de vida a los animalitos que estan
-            abandonados en la calle. Además contamos con un e-shop donde podes
-            comprarle comida, juguetes y muchas mas cosas a tu peludito. Con las
-            ganancias del e-shop, nosotros nos podemos financiar en mejorar
-            nuestro servicio y tambien ayudar a refugios y fundaciones de
-            animalitos.
-          </p>
-        </div> */}
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
