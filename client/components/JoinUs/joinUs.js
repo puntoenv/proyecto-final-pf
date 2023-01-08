f5bfd69e1939f81428ceb9e12b6a687970594ba4
maguiromeroa <maguiromeroa@gmail.com>
2023-01-08 11:19:21 +0100

    home css

---
+++ client/components/JoinUs/joinUs.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./join.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import Cat from "../../img/cat.png";
+
+const Join = () => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.join}>
+        Sumate a nuestra comunidad para poder adoptar o ayudar a las mascotas
+      </p>
+
+      <button className={styles.joinBtn}>
+        <Link href="/login">Sumarme </Link>
+      </button>
+      
+        <Image className={styles.head} src={Cat} width="auto" height="auto" />
+      
+    </div>
+    <div> <Image className={styles.dog} src="https://gfycat.com/ifr/AgileThoroughBarnacle" width="auto" height="auto" /> </div>
+  );
+};
+
+export default Join;
