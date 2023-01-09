db57d6ac09cc88ab9d790c38a8dea86c51a39a16
Emiliano1128 <emibrito100@icloud.com>
2023-01-09 19:16:14 -0300

    estructura del componente CardProduct lista

---
+++ client/components/CardProduct/index.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./styles.module.css";
+
+export default function CardProduct({ id, nombre, imagen, precio }) {
+  return (
+    <div className={styles.card}>
+      <Link href={`/detail/${id}`}>
+        <h3>{nombre.toUpperCase()}</h3>
+      </Link>
+      <Image src={imagen} width={100} height={100} alt="imagen de la mascota" />
+      {precio ? <p>Precio: {precio}</p> : null}
+    </div>
+  );
+}
