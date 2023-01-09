68eae827cf458cb3726c0d9dd9786b6d5d1319bf
Ezequiel Gómez Merlo <gomezmerloe@gmail.com>
2023-01-09 00:40:11 -0300

    ignore commit

--- client/pages/home/index.jsx
+++ client/pages/home/index.jsx
@@ -10,50 +10,6 @@ import home from "../../img/prueba.jpeg";
 import logo from "../../img/logo.jpeg";
 import Cat from "../../img/meal.png";
 import Join from "../../components/JoinUs/joinUs";
-<<<<<<< HEAD
-
-export default function Home() {
-  const clientId = process.env.AUHT0_CLIENT_ID;
-  return (
-    <>
-      <Layout title="Inicio" />
-      <div className={styles.home}>
-        <NavBar />
-        <Link href={"/home"} classname="logo">
-          <Image
-            src={logo}
-            alt="logo"
-            className={styles.logo}
-            width="auto"
-            height="auto"
-          />
-        </Link>
-        <p className={styles.friend}>Encuentra a tu nuevo mejor amigo</p>
-        <Image
-          src={home}
-          alt="home"
-          className={styles.home}
-          width="auto"
-          height="auto"
-        />
-        <div className={styles.barGreen}></div>
-        <div className={styles.divAlimento}></div>
-        {/* <span className={styles.catAlimento}></span> */}
-        <div className={styles.containKitten}>
-          <Image
-            src={Cat}
-            alt="kitten"
-            className={styles.kitten}
-            width="auto"
-            height="auto"
-          />
-        </div>
-=======
-import Alimento from "../../img/alimento.png";
-import Alim from "../../img/alim.png";
-import Al from "../../img/al.png";
-import Ali from "../../img/ali.png";
-import Alimen from "../../img/alimen.png";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -72,7 +28,6 @@ export default function Home() {
       slidesToShow: 3,
       slidesToScroll: 3,
     };
->>>>>>> main
 
     const data = useSelector((data) => data.mascotas.mascotas);
 
@@ -86,7 +41,7 @@ export default function Home() {
         <Layout title="Inicio" />
         <div className={styles.home}>
           <NavBar />
-          <Link href={"/home"} classname="logo">
+          <Link href={"/home"} className="logo">
             <Image
               src={logo}
               alt="logo"
@@ -118,39 +73,35 @@ export default function Home() {
 
           <Nosotros />
 
-          
-            <Slider {...settings}>
-              {data.slice(0, 9).map((mascota) => (
-                <PetsCard
-                  key={mascota._id}
-                  nombre={mascota.name}
-                  imagen={mascota.image}
-                  genero={mascota.gender}
-                />
-              ))}
-            </Slider>
-            <div className={styles.containerAdopciones}>
-          <h1 className={styles.tituloAdopcion}>Info Adopciones</h1>
-          <p className={styles.infoAdopcion}>
-            Nuestra plataforma permite a los usuarios visualizar todas las
-            mascotas disponibles para adopción, como así también filtar las
-            mismas por tamaño, ubicación, genero, etc. Tener una mascota es una
-            responsabilidad, por lo tanto, exigimos compromiso al momento de
-            adoptar a un nuevo integrante. También esperamos compromiso para con
-            los usuarios que llevan a cabo las publicaciones de los posteos.
-          </p>
+          <Slider {...settings}>
+            {data.slice(0, 9).map((mascota) => (
+              <PetsCard
+                key={mascota._id}
+                nombre={mascota.name}
+                imagen={mascota.image}
+                genero={mascota.gender}
+              />
+            ))}
+          </Slider>
+          <div className={styles.containerAdopciones}>
+            <h1 className={styles.tituloAdopcion}>Info Adopciones</h1>
+            <p className={styles.infoAdopcion}>
+              Nuestra plataforma permite a los usuarios visualizar todas las
+              mascotas disponibles para adopción, como así también filtar las
+              mismas por tamaño, ubicación, genero, etc. Tener una mascota es
+              una responsabilidad, por lo tanto, exigimos compromiso al momento
+              de adoptar a un nuevo integrante. También esperamos compromiso
+              para con los usuarios que llevan a cabo las publicaciones de los
+              posteos.
+            </p>
           </div>
           <Join />
 
           <Footer />
         </div>
-<<<<<<< HEAD
-
         <button onClick={() => console.log(clientId)}>.env</button>
-      </div>
-=======
-      
->>>>>>> main
-    </>
-  );
-}}
+            
+      </>
+    );
+  }
+}
