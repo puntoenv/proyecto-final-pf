0b70a3b15f13bf2418e909e70d542a06ea61c19b
saradehi <sarahidms@gmail.com>
2023-01-15 19:02:17 -0300

    cambios en rutas para local host

--- client/components/Profile/[id].jsx
+++ client/components/Profile/[id].jsx
@@ -85,7 +85,7 @@ export default function Perfil({
                         response.bio ? (
                           response.bio
                         ) : (
-                          "No especificado"
+                          "No especificado "
                         )
                       ) : (
                         <form
