780ea9830dd7f2feb48ac8d3574b080ae9be3f18
Ezequiel Gómez Merlo <gomezmerloe@gmail.com>
2023-01-20 18:20:00 -0300

    ingore commit

--- client/pages/detail/[id].jsx
+++ client/pages/detail/[id].jsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
 import styles from "./detail.module.css";
 import NavBar from "../../components/NavBar/NavBar";
 import Layout from "../layout";
 import Footer from "../../components/Footer/footer";
 import Link from "next/link";
-import logo from "../../img/logo.jpeg";
 import { useRouter } from "next/router";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import Swal from "sweetalert2/dist/sweetalert2";
@@ -73,38 +71,37 @@ export default function Detail({ data }) {
               <b>Tamaño: </b>
               <span>{data.size}</span>
             </div>
+
             <div class={styles.divSpecie}>
               <b>Especie: </b>
               <span>{data.type}</span>
             </div>
+
             <div class={styles.divCondition}>
               <b>Condición: </b>
               <span>{data.condition}</span>
             </div>
-            <div class={styles.divProvince}>
-              <b>Provincia: </b>
-              <span>{data.location.provincia}</span>
-            </div>
-            <div class={styles.divCity}>
-              <b>Ciudad: </b>
-              <span>{data.location.municipio}</span>
-            </div>
+
             <div class={styles.divGender}>
               <b>Genero: </b>
               <span>{data.gender}</span>
             </div>
+
             <div class={styles.divAge}>
               <b>Edad: </b>
               <span>{data.age}</span>
             </div>
+
             <div class={styles.divSocial}>
               <b>Interacción con otros animales: </b>
               <span>{data.sociability}</span>
             </div>
+
             <div class={styles.divHealth}>
               <b>Salud: </b>
               <span>{data.health}</span>
             </div>
+
             {data.healthExtra && (
               <div>
                 <b>Descripción de salud: </b>
