117eee7cba7fe0d7340b109a46b6f5eb613cfe53
Miranda <mirufuertes@gmail.com>
2023-01-22 23:11:54 -0300

    carousel pets

--- client/components/Carousel/petsCard.js
+++ client/components/Carousel/petsCard.js
@@ -5,21 +5,22 @@ export default function PetsCard({ id, nombre, imagen, genero, tamano }) {
   return (
     <div className={styles.big_container}>
       <div className={styles.imgcontainer}>
-         <Link href={`/detail/${id}`}>  
+         {/* <Link href={`/detail/${id}`}>   */}
           <img
             className={styles.image}
             src={imagen[0]}
-            // width={250}
-            // height={300}
             alt="imagen de la mascota"
           />
-          </Link>  
+          {/* </Link>   */}
           </div>
       
       <div className={styles.containInfo}>
         <h3 className={styles.name}> {nombre.toUpperCase()}</h3>
         <h1 className={styles.gender}> {genero}</h1>
-        <h1 className={styles.size}>{tamano}</h1>
+        {/* <h1 className={styles.size}>{tamano}</h1> */}
+        <button className={styles.btn}>
+                      <Link href={`/detail/${id}`}>Ver detalle</Link>
+                    </button>
       </div>
     </div>
   );
