fb2f428484490d0714bbf304c7769ffec6ac354c
maguiromeroa <maguiromeroa@gmail.com>
2023-01-25 03:42:53 +0100

    grids CSS, close add product

--- client/components/admin/Products/Products.jsx
+++ client/components/admin/Products/Products.jsx
@@ -1,5 +1,5 @@
 import { adminProducts } from "../../../stores/actions";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState, useRef, memo } from "react";
 import { Avatar, Box } from "@mui/material";
 import {
   DataGrid,
@@ -18,12 +18,14 @@ import Button from "@mui/material/Button";
 import Add from "./add";
 import PropTypes from "prop-types";
 import AddIcon from "@mui/icons-material/Add";
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';;
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import Rating from "@mui/material/Rating";
-//import styles from "../../Profile/Loading.module.css";
-import Pagination from '@mui/material/Pagination';
+import Pagination from "@mui/material/Pagination";
 import style from "./styles.module.css";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
 
 function CustomPagination() {
   const apiRef = useGridApiContext();
@@ -32,19 +34,143 @@ function CustomPagination() {
 
   return (
     <Pagination
-      color="primary"
+      sx={{ color: "#574c3d" }}
       count={pageCount}
       page={page + 1}
       onChange={(event, value) => apiRef.current.setPage(value - 1)}
     />
   );
 }
-function renderRating(params) {
-  return <Rating readOnly value={params.value} />;
+// function renderRating(params) {
+//   return <Rating readOnly value={params.value} />;
+// }
+
+// renderRating.propTypes = {
+//   value: PropTypes.number,
+// };
+function isOverflown(element) {
+  return (
+    element.scrollHeight > element.clientHeight ||
+    element.scrollWidth > element.clientWidth
+  );
 }
 
-renderRating.propTypes = {
-  value: PropTypes.number,
+const GridCellExpand = memo(function GridCellExpand(props) {
+  const { width, value } = props;
+  const wrapper = useRef(null);
+  const cellDiv = useRef(null);
+  const cellValue = useRef(null);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [showFullCell, setShowFullCell] = useState(false);
+  const [showPopper, setShowPopper] = useState(false);
+
+  const handleMouseEnter = () => {
+    const isCurrentlyOverflown = isOverflown(cellValue.current);
+    setShowPopper(isCurrentlyOverflown);
+    setAnchorEl(cellDiv.current);
+    setShowFullCell(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowFullCell(false);
+  };
+
+  useEffect(() => {
+    if (!showFullCell) {
+      return undefined;
+    }
+
+    function handleKeyDown(nativeEvent) {
+    
+      if (nativeEvent.key === "Escape" || nativeEvent.key === "Esc") {
+        setShowFullCell(false);
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [setShowFullCell, showFullCell]);
+
+  return (
+    <Box
+      ref={wrapper}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      sx={{
+        alignItems: "center",
+        lineHeight: "24px",
+        width: "100%",
+        height: "100%",
+        position: "relative",
+        display: "flex",
+        
+      }}
+    >
+      <Box
+        ref={cellDiv}
+        sx={{
+          height: "100%",
+          width,
+          display: "block",
+          position: "absolute",
+          top: 0,
+         
+        }}
+      />
+      <Box
+        ref={cellValue}
+        sx={{
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          typography: 'subtitle2',
+          textAlign: 'center',
+          textTransform: 'capitalize'
+        }}
+      >
+        {value}
+      </Box>
+      {showPopper && (
+        <Popper
+          open={showFullCell && anchorEl !== null}
+          anchorEl={anchorEl}
+          style={{ width, marginLeft: -17, }}
+        >
+          <Paper
+            elevation={1}
+            style={{ minHeight: wrapper.current.offsetHeight - 3, backgroundColor: "rgba(42, 41, 37, 0.942)", }}
+          >
+            <Typography variant="body2" style={{ padding: 8, color: "white" }}>
+              {value}
+            </Typography>
+          </Paper>
+        </Popper>
+      )}
+    </Box>
+  );
+});
+
+GridCellExpand.propTypes = {
+  value: PropTypes.string.isRequired,
+  width: PropTypes.number.isRequired,
+};
+
+function renderCellExpand(params) {
+  return (
+    <GridCellExpand
+      value={params.value || ""}
+      width={params.colDef.computedWidth}
+    />
+  );
+}
+
+renderCellExpand.propTypes = {
+  colDef: PropTypes.object.isRequired,
+
+  value: PropTypes.string,
 };
 
 export default function Products() {
@@ -59,12 +185,6 @@ export default function Products() {
     setRender();
   }, [dispatch]);
 
-
-
-
-
-
-
   const currencyFormatter = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
@@ -77,43 +197,92 @@ export default function Products() {
     cellClassName: "font-tabular-nums",
   };
 
-  const status = () => {
-   "PRUEBA"
-    }
+  // const category = () =>
+  //   products.map((product) => {
+  //     product.category.length > 0 ? product.category : "Sin categoria/s";
+  //   });
 
+  function Blocked(props) {
+    return (
+      <div className={style.red}>
+        <VisibilityOffIcon className={style.iconOff} {...props}>
+          <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />{" "}
+        </VisibilityOffIcon>
+        <h3 className={style.blocked}>Oculto</h3>
+      </div>
+    );
+  }
 
-const category = ()=>
-products.map((product)=>{
-  product.category.length > 0 ? product.category : "Sin categoria/s"
-})
-
+  function Unblocked(props) {
+    return (
+      <div className={style.green}>
+        <VisibilityIcon className={style.icon} {...props}>
+          <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />{" "}
+        </VisibilityIcon>
+        <h3 className={style.active}>Activo</h3>
+      </div>
+    );
+  }
 
   const columns = useMemo(() => [
     {
-      field: "image",
-      headerName: "Imagen",
-      width: 60,
+      field: " ",
+      headerName: " ",
+      width: 50,
+      headerClassName: "super-app-theme--header",
       renderCell: (params) => <Avatar src={params.row.image} />,
       sortable: false,
       filterable: false,
+      headerAlign: "center",
     },
-    { field: "name", headerName: "Name", editable: true, width: 160 },
-    { field: "hidden", headerName: "Estado", editable: true, width: 100 },
+    {
+      field: "name",
+      headerClassName: "super-app-theme--header",
+      headerName: "Nombre",
+      headerAlign: "left",
+      editable: true,
+      width: 140, renderCell: renderCellExpand,
+    },
+    {
+      field: "hidden",
+      headerName: "Estado",
+      headerClassName: "super-app-theme--header",
+      headerAlign: "left",
+      width: 135,
+      renderCell: (params) =>
+        params.row.hidden === true ? (
+          <Blocked sx={{ color: "#ff2525" }} src={params.row.hidden} />
+        ) : (
+          <Unblocked sx={{ color: "green" }} src={params.row.hidden} />
+        ),
+      sortable: false,
+      filterable: false,
+    },
     {
       field: "category",
       headerName: "Categorias",
-      
+      headerAlign: "center",
       editable: true,
-      width: 120,
+      headerClassName: "super-app-theme--header",
+      width: 120, renderCell: renderCellExpand,
     },
     {
       field: "price",
       headerName: "Precio",
       ...usdPrice,
+      headerAlign: "center",
+      headerClassName: "super-app-theme--header",
       editable: true,
       width: 120,
     },
-    { field: "stock", headerName: "Stock", editable: true, width: 120 },
+    {
+      field: "stock",
+      headerAlign: "center",
+      headerClassName: "super-app-theme--header",
+      headerName: "Stock",
+      editable: true,
+      width: 80, renderCell: renderCellExpand, 
+    },
     // {
     //   field: '',
     //   headerName: "Rating",
@@ -124,10 +293,19 @@ products.map((product)=>{
     {
       field: "description",
       headerName: "Descripción",
+      headerAlign: "center",
+      headerClassName: "super-app-theme--header",
       editable: true,
       width: 300,
+      renderCell: renderCellExpand,
     },
-    { field: "_id", headerName: "Id", width: 160 },
+    {
+      field: "_id",
+      headerAlign: "center",
+      headerClassName: "super-app-theme--header",
+      headerName: "Id",
+      width: 160, 
+    },
   ]);
 
   const handleHide = async () => {
@@ -165,6 +343,7 @@ products.map((product)=>{
     return (
       <GridToolbarContainer>
         <Button
+          sx={{ color: "#574c3d" }}
           color="primary"
           startIcon={<AddIcon />}
           onClick={() => handleAdd(<Add />)}
@@ -172,15 +351,23 @@ products.map((product)=>{
           Agregar
         </Button>
 
-        <Button startIcon={<VisibilityOffIcon />} onClick={(e) => handleHide(e)}>
+        <Button
+          sx={{ color: "#574c3d" }}
+          startIcon={<VisibilityOffIcon />}
+          onClick={(e) => handleHide(e)}
+        >
           Ocultar
         </Button>
-        <Button startIcon={<VisibilityIcon />} onClick={(e) => handleShow(e)}>
+        <Button
+          sx={{ color: "#574c3d" }}
+          startIcon={<VisibilityIcon />}
+          onClick={(e) => handleShow(e)}
+        >
           Mostrar
         </Button>
-        <GridToolbarColumnsButton />
-        <GridToolbarFilterButton />
-        <GridToolbarDensitySelector />
+        <GridToolbarColumnsButton sx={{ color: "#574c3d" }} />
+        <GridToolbarFilterButton sx={{ color: "#574c3d" }} />
+        <GridToolbarDensitySelector sx={{ color: "#574c3d" }} />
       </GridToolbarContainer>
     );
   }
@@ -193,22 +380,23 @@ products.map((product)=>{
 
       <Box
         sx={{
-          height: 460,
+          height: 600,
           width: "96%",
-          
+          "& .super-app-theme--header": {
+            backgroundColor: "rgba(77, 64, 40, 0.14)",
+          },
         }}
       >
         <h1 className={style.products}> Productos </h1>
 
         <DataGrid
-         pagination
+          pagination
           onRowClick={(e) => setData(e)}
           sx={{ ml: 35 }}
           columns={columns}
           rows={products}
           editMode="cell"
           getRowId={(row) => row._id}
-          getRowHeight={() => 'auto'}
           rowsPerPageOptions={[5, 10, 20]}
           pageSize={pageSize}
           onCellEditStop={handleEdit}
@@ -222,7 +410,7 @@ products.map((product)=>{
             Toolbar: CustomToolbar,
             Pagination: CustomPagination,
           }}
-        
+
           // experimentalFeatures={{ newEditingApi: true }}
         />
       </Box>
--- client/components/admin/Users/Users.jsx
+++ client/components/admin/Users/Users.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState, useRef, memo  } from "react";
 import { Avatar, Box } from "@mui/material";
 import {
   DataGrid,
@@ -6,10 +6,6 @@ import {
   GridToolbarColumnsButton,
   GridToolbarFilterButton,
   GridToolbarDensitySelector,
-  gridPageCountSelector,
-  gridPageSelector,
-  useGridApiContext,
-  useGridSelector,
 } from "@mui/x-data-grid";
 import { allUsers } from "../../../stores/actions";
 import { useSelector, useDispatch } from "react-redux";
@@ -17,65 +13,252 @@ import style from "./style.module.css";
 import Button from "@mui/material/Button";
 import PersonOffIcon from "@mui/icons-material/PersonOff";
 import PersonIcon from "@mui/icons-material/Person";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
+import PropTypes from "prop-types";
+
+
+
+
+function isOverflown(element) {
+  return (
+    element.scrollHeight > element.clientHeight ||
+    element.scrollWidth > element.clientWidth
+  );
+}
+
+const GridCellExpand = memo(function GridCellExpand(props) {
+  const { width, value } = props;
+  const wrapper = useRef(null);
+  const cellDiv = useRef(null);
+  const cellValue = useRef(null);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [showFullCell, setShowFullCell] = useState(false);
+  const [showPopper, setShowPopper] = useState(false);
+
+  const handleMouseEnter = () => {
+    const isCurrentlyOverflown = isOverflown(cellValue.current);
+    setShowPopper(isCurrentlyOverflown);
+    setAnchorEl(cellDiv.current);
+    setShowFullCell(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowFullCell(false);
+  };
+
+  useEffect(() => {
+    if (!showFullCell) {
+      return undefined;
+    }
+
+    function handleKeyDown(nativeEvent) {
+    
+      if (nativeEvent.key === "Escape" || nativeEvent.key === "Esc") {
+        setShowFullCell(false);
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [setShowFullCell, showFullCell]);
+
+  return (
+    <Box
+      ref={wrapper}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      sx={{
+        alignItems: "center",
+        lineHeight: "24px",
+        width: "100%",
+        height: "100%",
+        position: "relative",
+        display: "flex",
+        
+      }}
+    >
+      <Box
+        ref={cellDiv}
+        sx={{
+          height: "100%",
+          width,
+          display: "block",
+          position: "absolute",
+          top: 0,
+         
+        }}
+      />
+      <Box
+        ref={cellValue}
+        sx={{
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          typography: 'subtitle2',
+          textAlign: 'center',
+          textTransform: 'capitalize'
+        }}
+      >
+        {value}
+      </Box>
+      {showPopper && (
+        <Popper
+          open={showFullCell && anchorEl !== null}
+          anchorEl={anchorEl}
+          style={{ width, marginLeft: -17, }}
+        >
+          <Paper
+            elevation={1}
+            style={{ minHeight: wrapper.current.offsetHeight - 3, backgroundColor: "rgba(42, 41, 37, 0.942)", }}
+          >
+            <Typography variant="body2" style={{ padding: 8, color: "white" }}>
+              {value}
+            </Typography>
+          </Paper>
+        </Popper>
+      )}
+    </Box>
+  );
+});
+
+GridCellExpand.propTypes = {
+  value: PropTypes.string.isRequired,
+  width: PropTypes.number.isRequired,
+};
+
+function renderCellExpand(params) {
+  return (
+    <GridCellExpand
+      value={params.value || ""}
+      width={params.colDef.computedWidth}
+    />
+  );
+}
+
+renderCellExpand.propTypes = {
+  colDef: PropTypes.object.isRequired,
+
+  value: PropTypes.string,
+};
+
+
+
 
 export default function Users() {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.user.users);
 
-  const [pageSize, setPageSize] = useState(5);
+  const [pageSize, setPageSize] = useState(8);
   const [data, setData] = useState();
 
   useEffect(() => {
     dispatch(allUsers());
   }, [dispatch]);
 
- 
- 
-    const usersNew = [];
-    for (let i = 0; i < users.length; i++) {
-      usersNew.push({
-        name: users[i].name,
-        email: users[i].email,
-        image: users[i].image,
-        posts: users[i].pets.length,
-        bought: users[i].bought,
-        _id: users[i]._id,
-        hidden: users[i].hidden,
-        age: users[i].age,
-        directions: users[i].directions,
-        merchant_orders: users[i].merchant_orders,
-      });
-    }
+  const usersNew = [];
+  for (let i = 0; i < users.length; i++) {
+    usersNew.push({
+      name: users[i].name,
+      email: users[i].email,
+      image: users[i].image,
+      posts: users[i].pets.length,
+      bought: users[i].bought,
+      _id: users[i]._id,
+      hidden: users[i].hidden,
+      age: users[i].age,
+      directions: users[i].directions,
+      merchant_orders: users[i].merchant_orders,
+    });
+  }
 
-    
- 
-  
-  // const copy = async () => {
-  //   const newUsers = [];
-  //   const usuarios = [...users];
-  //   for (let i = 0; i < usuarios; i++) {
-  //     const prop = await (usuarios[i].posts = usuarios[i].pets.length);
-  //     const more = usuarios[i] = prop
-  //     newUsers.push(more);
-  //   }
-  //   console.log(newUsers);
-  // };
-  // copy();
+  function Blocked(props) {
+    return (
+      <div className={style.red}>
+        <PersonOffIcon className={style.iconOff} {...props}>
+          <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />{" "}
+        </PersonOffIcon>
+        <h3 className={style.blocked}>Bloqueado</h3>
+      </div>
+    );
+  }
+
+  function Unblocked(props) {
+    return (
+      <div className={style.green}>
+        <PersonIcon className={style.icon} {...props}>
+          <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />{" "}
+        </PersonIcon>
+        <h3 className={style.active}>Activo</h3>
+      </div>
+    );
+  }
 
   const columns = useMemo(() => [
     {
       field: "image",
-      headerName: "Imagen",
-      width: 60,
+      headerName: " ",
+      headerClassName: "super-app-theme--header",
+      width: 50,
       renderCell: (params) => <Avatar src={params.row.image} />,
       sortable: false,
       filterable: false,
     },
-    { field: "name", headerName: "Name", width: 140 },
-    { field: "email", headerName: "Email", width: 200 },
-    { field: "posts", headerName: "Posts", width: 220 },
-    { field: "bought", headerName: "Compras", width: 220 },
-    { field: "_id", headerName: "Id", width: 220 },
+    {
+      field: "name",
+      headerClassName: "super-app-theme--header",
+      headerName: "Nombre ",
+      headerAlign: "left",
+      width: 140, renderCell: renderCellExpand, 
+    },
+
+    {
+      field: "hidden",
+      headerName: "Estado",
+      headerClassName: "super-app-theme--header",
+      headerAlign: "left",
+      width: 120,
+      renderCell: (params) =>
+        params.row.hidden === true ? (
+          <Blocked sx={{ color: "#ff2525" }} src={params.row.hidden} />
+        ) : (
+          <Unblocked sx={{ color: "green" }} src={params.row.hidden} />
+        ),
+      sortable: false,
+      filterable: false,
+    },
+    {
+      field: "email",
+      headerName: "Mail",
+      headerClassName: "super-app-theme--header",
+      headerAlign: "left",
+      width: 200,
+    },
+    {
+      field: "posts",
+      headerName: "Posts",
+      headerClassName: "super-app-theme--header",
+      headerAlign: "left",
+      width: 50,
+    },
+    {
+      field: "bought",
+      headerName: "Compras",
+      headerClassName: "super-app-theme--header",
+      headerAlign: "center",
+      width: 220,
+    },
+    {
+      field: "_id",
+      headerName: "Id",
+      headerClassName: "super-app-theme--header",
+      headerAlign: "center",
+      width: 150,
+    },
   ]);
 
   const handleHide = async () => {
@@ -98,15 +281,23 @@ export default function Users() {
   function CustomToolbar() {
     return (
       <GridToolbarContainer>
-        <Button startIcon={<PersonOffIcon />} onClick={(e) => handleHide(e)}>
+        <Button
+          sx={{ color: "#574c3d" }}
+          startIcon={<PersonOffIcon />}
+          onClick={(e) => handleHide(e)}
+        >
           Bloquear
         </Button>
-        <Button startIcon={<PersonIcon />} onClick={(e) => handleShow(e)}>
+        <Button
+          sx={{ color: "#574c3d" }}
+          startIcon={<PersonIcon />}
+          onClick={(e) => handleShow(e)}
+        >
           Desbloquear
         </Button>
-        <GridToolbarColumnsButton />
-        <GridToolbarFilterButton />
-        <GridToolbarDensitySelector />
+        <GridToolbarColumnsButton sx={{ color: "#574c3d" }} />
+        <GridToolbarFilterButton sx={{ color: "#574c3d" }} />
+        <GridToolbarDensitySelector sx={{ color: "#574c3d" }} />
       </GridToolbarContainer>
     );
   }
@@ -114,8 +305,11 @@ export default function Users() {
   return (
     <Box
       sx={{
-        height: 460,
+        height: 600,
         width: "96%",
+        "& .super-app-theme--header": {
+          backgroundColor: "rgba(77, 64, 40, 0.14)",
+        },
       }}
     >
       <h1 className={style.users}> Usuarios </h1>
